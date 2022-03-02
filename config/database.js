32b95caf709b389f524c4a6b974b372cc976247b
devrayat000 <2010050@me.buet.ac.bd>
2022-03-02 21:58:20 +0600

    solved database url env

--- config/database.js
+++ config/database.js
@@ -1,13 +1,24 @@
 module.exports = ({ env }) => {
+  const dburlRegex =
+    /postgres:\/\/(?<username>\w+):(?<password>\w+)@(?<host>[a-zA-z0-9\-\._]+):(?<port>\d+)\/(?<database>\w+)/;
+  const dburl = env(
+    "DATABASE_URL",
+    "postgres://postgres:ppooii12@localhost:5432/strapi_buet_me"
+  );
+
+  const {
+    groups: { username, password, host, port, database },
+  } = dburlRegex.exec(dburl);
+
   return {
     connection: {
       client: "postgres",
       connection: {
-        host: env("DATABASE_HOST", "127.0.0.1"),
-        port: env.int("DATABASE_PORT", 5432),
-        database: env("DATABASE_NAME", "strapi_buet_me"),
-        user: env("DATABASE_USERNAME", "postgres"),
-        password: env("DATABASE_PASSWORD", "ppooii12"),
+        host: env("DATABASE_HOST", host),
+        port: env.int("DATABASE_PORT", parseInt(port)),
+        database: env("DATABASE_NAME", database),
+        user: env("DATABASE_USERNAME", username),
+        password: env("DATABASE_PASSWORD", password),
         ssl: env.bool("DATABASE_SSL", false),
       },
     },
