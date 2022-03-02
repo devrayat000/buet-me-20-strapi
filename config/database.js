29a74443a5233e6eeca62213adc60ba709ac0931
devrayat000 <2010050@me.buet.ac.bd>
2022-03-02 22:31:46 +0600

    ssl prob 2

--- config/database.js
+++ config/database.js
@@ -19,8 +19,14 @@ module.exports = ({ env }) => {
         database: env("DATABASE_NAME", database),
         user: env("DATABASE_USERNAME", username),
         password: env("DATABASE_PASSWORD", password),
-        ssl: env.bool("DATABASE_SSL", false),
+        ssl: {
+          rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
+        },
       },
     },
   };
 };
+/**
+ * postgres://pviufhqkkwenhw:ac95399e65a7e88ed724d6d37b18931ce757fb5a0b0688f5210b4ce92321b250
+ * @ec2-54-83-21-198.compute-1.amazonaws.com:5432/ddpgo0l3u7u1i2
+ */
